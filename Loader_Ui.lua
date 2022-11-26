0fc9eb20493bbf7ea262ddcd1503102e77ba361d
Pcall Hub <119106203+PcallHub@users.noreply.github.com>
2022-11-26 18:12:06 +0800

    Update and rename Loader_Ui to Loader_Ui.lua

--- Loader_Ui.lua
+++ Loader_Ui.lua
@@ -1,3 +1,5 @@
 if game.PlaceId == 2753915549 or 4442272183 or 7449423635 then
    loadstring(game:HttpGet(('https://raw.githubusercontent.com/PcallHub/Script/main/Games/Games_BloxFruit')))()
+else
+   game.Players.LocalPlayer:Kick("Not Supported!\ndiscord.gg/8W8T6vcZZG")
 end
